5ce48aff30b8a6897da74311028cae8b1f16853a
shen <santi.dionis@gmail.com>
2023-04-21 20:53:40 +0200

    ADD: Retos

--- reto-11.js
+++ reto-11.js
@@ -3,4 +3,9 @@ const getName = (obj) => {
     return obj;
   };
   console.log(getName({}));
-  
+  Respuesta Correcta: C) { name:"Sin Nombre" }
+El operador ??= se llama Logical Nullish Assignment es un operador de corto circuito moderno que consiste en ejecutar porciones de código si evaluamos una condición como nullish, osea, como valor null o undefined.
+
+Entonces, en el ejemplo, si obj.name evalua como nullish, ejecutamos "Sin Nombre".
+
+Llamamos a la función getName pasandole un objeto vacío, entonces todas sus propiedades son undefined y por consecuencia nullish, por ello a obj.name se el asigna el valor "Sin Nombre" y retornamos ese objeto. 
