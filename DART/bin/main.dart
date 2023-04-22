35badeeca15390ff56f9705c562587e995203714
shen <santi.dionis@gmail.com>
2023-04-22 21:51:08 +0200

    ADD: Dart

--- DART/bin/main.dart
+++ DART/bin/main.dart
@@ -1,5 +1,15 @@
 void main() {
+  /*
   for (var i = 0; i < 10; i++) {
     print('hello ${i + 1}');
+  }*/
+  print("Programa para saber si esta soleado o no");
+  bool estasonando = true;
+  print(estasonando);
+  if (estasonando) {
+    print("Esta soleado");
+  } else {
+    print("No esta soleado");
   }
 }
+
