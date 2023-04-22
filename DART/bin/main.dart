63b8dbab1b3cc6f1f471a26b54e3581b2ad00a0c
shen <santi.dionis@gmail.com>
2023-04-22 22:00:52 +0200

    ADD: Dart

--- DART/bin/main.dart
+++ DART/bin/main.dart
@@ -2,7 +2,7 @@ void main() {
   /*
   for (var i = 0; i < 10; i++) {
     print('hello ${i + 1}');
-  }*/
+  }
   print("Programa para saber si esta soleado o no");
   bool estasonando = true;
   print(estasonando);
@@ -10,6 +10,27 @@ void main() {
     print("Esta soleado");
   } else {
     print("No esta soleado");
+  }*/
+  print("Programa para saber la edad y estatura");
+  int edad = 18;
+  double estatura = 1.70;
+  print("La edad es ${edad} y la estatura es ${estatura}");
+  if (edad >= 18 && estatura >= 1.70) {
+    print("Puedes entrar al bar");
+  } else {
+    print("No puedes entrar al bar");
   }
+
+  //tipos datos
+  String pais = "España";
+  List <String> paises = ["España", "Mexico", "Colombia"];
+  
+  print(pais);
+  print(paises[0]);
+  print(paises.first);
+  print(paises.last);
+
+
+
 }
 
