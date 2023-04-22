6c64a6e6077e97cb04053518046008b0a3c17267
shen <santi.dionis@gmail.com>
2023-04-22 13:12:19 +0200

    ADD: Retos

---
+++ reto-12.js
@@ -0,0 +1,7 @@
+const fruits = [
+    { id: 1, name: 'Lime', quantity: 3 },
+    { id: 2, name: 'Apple', quantity: 2 },
+    { id: 3, name: 'Pineapple', quantity: 4 },
+  ];
+  
+  const count = fruits.some((fruit) => fruit.quantity > 5);
