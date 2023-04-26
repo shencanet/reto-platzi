afc5fdbedbdee563508f13a959dc56cb05c790bf
shen <santi.dionis@gmail.com>
2023-04-26 19:53:11 +0200

    ADD: Retos

---
+++ reto-16.js
@@ -0,0 +1,9 @@
+const banda = {
+    id: 1,
+    nombre: 'Radiohead',
+    'tipo de musica': 'Rock',
+    albunes: ['Pablo Honey', 'Ok Computer', 'In Rainbows'],
+  };
+  console.log(banda['nom' + 'bre']);
+  console.log(banda['i' + 'd']);
+  console.log(banda['alb' + 'unes'][0]);
