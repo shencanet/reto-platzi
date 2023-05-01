b7280918b455c9c195057e49f2851ae1c9fc7311
shen <santi.dionis@gmail.com>
2023-05-01 12:49:00 +0200

    ADD: Retos

--- reto-21.js
+++ reto-21.js
@@ -1,1 +1,26 @@
 console.log(undefined || '0' || null || (undefined ?? 0));
+
+Respuesta Correcta: B) "0"
+El operador de corto circuito OR (||) solo se ejecuta si el primer operando es falsy. El nullish coalescing operator (??) solo se ejecuta si el primer operando es nullish (null o undefined).
+
+Vamos paso por paso:
+
+undefined || "0": undefined evalua como falsy entonces tendriamos "0". Nos quedaria el siguiente código:
+
+  console.log("0" || null || (undefined ?? 0))  
+
+
+
+"0" || null: "0" no evalua como falsy entonces no se ejecuta el operador de corto circuito. Nos quedaria el siguiente código:
+
+  console.log("0" || (undefined ?? 0))  
+
+
+
+undefined ?? 0: Operando tenemos como resultado 0 por que undefined es un valor nullish. Nos quedaria el siguiente código:
+
+  console.log("0" || 0)  
+
+
+
+Finalmente "0" como cadena no es un valor falsy entonces no podemos ejecutar el operador de corto circuito dando como resultado final "0"
