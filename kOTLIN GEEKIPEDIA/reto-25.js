b6ea1626de5fb6bd818cb42c0d5ca4c6b129e6bc
shen <santi.dionis@gmail.com>
2023-05-07 15:27:24 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-25.js
+++ kOTLIN GEEKIPEDIA/reto-25.js
@@ -12,3 +12,9 @@ console.log(typeof Infinity == 'object'); // false
 console.log(typeof -Infinity == 'number'); // true
 console.log(typeof -Infinity == 'object'); // false
 console.log(typeof 1 == 'number'); // true
+
+/*Pese a que null es un primitivo, debido a un bug del lenguaje su tipo de dato es object.
+
+Este bug es muy antiguo y se determino que no vale la pena arreglarlo al día de hoy ya que se pueden romper muchos programas que pueden depender de esta error.
+
+Este bug es bastante conocido en programadores experimentados y usado en entrevistas laborales para estimar tu conocimiento del lenguaje.*/
