d9867517400236cdcabad86d47b1e5d84e692096
shen <santi.dionis@gmail.com>
2023-05-11 20:29:15 +0200

    ADD: Kotlin arrays

--- reto-28.js
+++ reto-28.js
@@ -1,2 +1,5 @@
 const numeros = [1, 2, [3, 4], 5, 6, [7, 8], 9, 0];
 console.log(numeros.flat()); // ?
+/*Respuesta Correcta: B) [1,2,3,4,5,6,7,8,9,0]
+flat es un array method que crea un nuevo arreglo con los elementos concatenados recursivamente hasta una profundidad especificada.
+Dicho en otras palabras, permite "aplanar" un arreglo anidado un número determinado de veces. Es una buena alernativa a usar por ejemplo reduce para hacer lo mismo y no muchos devs conocen que existe esta catacterística en el lenguaje.*/
