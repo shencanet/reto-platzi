665f6864f1e1943df5f36958eca56303c36e1021
shen <santi.dionis@gmail.com>
2023-05-12 21:22:05 +0200

    ADD: Kotlin arrays

---
+++ reto--029.js
@@ -0,0 +1,9 @@
+var nombre = 'Camila';
+let apellido = 'Rodriguez';
+const edad = 25;
+const getDatosPersonales = () => {
+  console.log(nombre);
+  console.log(apellido);
+  console.log(edad);
+};
+console.log(getDatosPersonales());
