a38e0f21b146494f33961189d7ac22a4238bb676
shen <santi.dionis@gmail.com>
2023-05-12 21:22:21 +0200

    ADD: Kotlin arrays

--- reto--029.js
+++ reto--029.js
@@ -7,3 +7,5 @@ const getDatosPersonales = () => {
   console.log(edad);
 };
 console.log(getDatosPersonales());
+
+/*Independientemente de la palabra reservada con la que declaremos una variable (var, let, const), esta tendrá scope global siempre y cuando no este dentro de un bloque o dentro de una función. Por este motivo nombre, apellido y edad son variables de scope global y por ello pueden ser accedidas desde la función getDatosPersonales*/
