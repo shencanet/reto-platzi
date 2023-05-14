c63b97f37b0809632ea0501c8972a66a7cc27661
shen <santi.dionis@gmail.com>
2023-05-14 19:36:45 +0200

    ADD: Retos

--- KOTLIN/reto-M-01.js
+++ KOTLIN/reto-M-01.js
@@ -4,3 +4,6 @@ const test = {
     for: 'Es un bucle',
   };
   console.log(test.for); //??
+  /*Dentro de un objeto literal es posible usar nombres de palabras reservadas del lenguaje como nombres de keys, esto es perfectamente valido.
+
+❌ Pese a que es valido, se recomienda no hacer esto y respetar las palabras reservadas de javascript. ¡No hagas nunca esto! Solo se conciente que es posible.*/
