34e948db50f9141a56863115da4c6c52a66f8a75
shen <santi.dionis@gmail.com>
2023-05-15 22:00:52 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/Reto_25.js
+++ kOTLIN GEEKIPEDIA/Reto_25.js
@@ -1,3 +1,7 @@
 let nombre = 'Cris';
 let edad = 25;
 console.log('Mi nombre es %s y tengo %d', nombre, edad);
+
+En javascript como en java es posible usar sustituciones de variables con el operador % seguido de un caracter que especifica el tipo de dato que se pretende imprimir. En este caso, $s reemplaza un string ("Cris") y %d reemplaza un valor decimal o dígito numérico (25).
+
+Este método de imprimir por consola no es muy usado, ni siquiera es conocido, pero esta bueno saber que existe.
