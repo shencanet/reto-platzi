8caedddb91ee933dfa88b7ba370937720e50b832
shen <santi.dionis@gmail.com>
2023-05-16 21:36:08 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-22b.js
+++ kOTLIN GEEKIPEDIA/reto-22b.js
@@ -1,3 +1,9 @@
 const nombres = ['Ana', 'Sofia', 'Carmen', ...['Cris']];
 const [, , , miNombre] = nombres;
 console.log(miNombre['length']);
+
+Cuando pretendemos hacer una desestructuración de arreglos es súper importante tener en cuenta los índices del mismo. Usando la sintaxis de la , podemos saltar posiciones del arreglo hasta encontrar la propiedad que se desea obtener.
+
+En el ejemplo usamos 3 veces , por ello saltamos 3 posiciones del arreglo nombres para poder obtener (con spread operator) la cadena Cris del arreglo anidado.
+
+Finalmente aplicamos el método length con sintaxis de corchete.
