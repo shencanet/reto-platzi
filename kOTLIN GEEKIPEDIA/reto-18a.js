dce0a1cbffc2a6d207342c5d497830edc2d3193e
shen <santi.dionis@gmail.com>
2023-05-18 20:15:21 +0200

    ADD: Retos

---
+++ kOTLIN GEEKIPEDIA/reto-18a.js
@@ -0,0 +1,6 @@
+const profesores = ['Oscar', 'Nico', 'Freddy', 'Christian', 'Angela'];
+console.log(profesores.at(1)); //?
+console.log(profesores.at(-1)); //?
+console.log(profesores.at(10)); //?
+console.log(profesores.at(3.8)); //?
+console.log(profesores.at(-3.3)); //?
