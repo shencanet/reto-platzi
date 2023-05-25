016abb72f572d1870a3c10db61d14f7930e6bfea
shen <santi.dionis@gmail.com>
2023-05-25 21:52:44 +0200

    ADD: Retos kotlin

---
+++ kOTLIN GEEKIPEDIA/reto.kotin01.kt
@@ -0,0 +1,2 @@
+Escribe una función en Kotlin llamada sumaDigitos que tome un número entero positivo como parámetro y devuelva la suma de sus dígitos. Por ejemplo, si se pasa el número 123, la función debe devolver 6 (1 + 2 + 3).
+
