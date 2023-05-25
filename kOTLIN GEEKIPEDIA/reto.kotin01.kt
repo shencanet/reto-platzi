2417b5abedaa50ea5ea7456bb61a4cf0bbc4b3b1
shen <santi.dionis@gmail.com>
2023-05-25 21:52:58 +0200

    ADD: Kotlin arrays

--- kOTLIN GEEKIPEDIA/reto.kotin01.kt
+++ kOTLIN GEEKIPEDIA/reto.kotin01.kt
@@ -1,2 +1,13 @@
 Escribe una función en Kotlin llamada sumaDigitos que tome un número entero positivo como parámetro y devuelva la suma de sus dígitos. Por ejemplo, si se pasa el número 123, la función debe devolver 6 (1 + 2 + 3).
 
+fun sumaDigitos(numero: Int): Int {
+    var suma = 0
+    var num = numero
+    
+    while (num != 0) {
+        suma += num % 10
+        num /= 10
+    }
+    
+    return suma
+}
