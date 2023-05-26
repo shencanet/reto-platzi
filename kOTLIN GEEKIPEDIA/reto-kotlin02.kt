5a141e5797313d06ed711804c318e60ee889395e
shen <santi.dionis@gmail.com>
2023-05-26 20:55:05 +0200

    ADD: Kotlin arrays

---
+++ kOTLIN GEEKIPEDIA/reto-kotlin02.kt
@@ -0,0 +1,2 @@
+Escribe una función en Kotlin llamada esPalindromo que tome una cadena de texto como parámetro y determine si es un palíndromo. Un palíndromo es una palabra, frase, número o cualquier otra secuencia de caracteres que se lee igual hacia adelante y hacia atrás, sin tener en cuenta los espacios en blanco, signos de puntuación ni mayúsculas y minúsculas.
+
