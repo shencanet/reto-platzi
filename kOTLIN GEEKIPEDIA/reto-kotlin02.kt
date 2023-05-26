b83748e31218f36e138d6a030249a9f0502f752f
shen <santi.dionis@gmail.com>
2023-05-26 20:55:14 +0200

    ADD: Kotlin arrays

--- kOTLIN GEEKIPEDIA/reto-kotlin02.kt
+++ kOTLIN GEEKIPEDIA/reto-kotlin02.kt
@@ -1,2 +1,12 @@
 Escribe una función en Kotlin llamada esPalindromo que tome una cadena de texto como parámetro y determine si es un palíndromo. Un palíndromo es una palabra, frase, número o cualquier otra secuencia de caracteres que se lee igual hacia adelante y hacia atrás, sin tener en cuenta los espacios en blanco, signos de puntuación ni mayúsculas y minúsculas.
 
+fun esPalindromo(texto: String): Boolean {
+    val textoFormateado = texto.replace("[^A-Za-z0-9]".toRegex(), "").toLowerCase()
+    val longitud = textoFormateado.length
+    for (i in 0 until longitud / 2) {
+        if (textoFormateado[i] != textoFormateado[longitud - i - 1]) {
+            return false
+        }
+    }
+    return true
+}
