afcfc24410604f1399481def904fcc24e5e1a387
shen <santi.dionis@gmail.com>
2023-05-28 16:17:04 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-28b.js
+++ kOTLIN GEEKIPEDIA/reto-28b.js
@@ -1,3 +1,11 @@
 !!null;
 !!'';
 !!1;
+
+El operador !! realiza una doble negación.
+
+En el primer caso, por coerción de tipos, null es un valor falsy, si lo negamos 2 veces, tendríamos false.
+
+En el segundo caso, por coerción de tipos, "" es un valor falsy, si lo negamos 2 veces tendríamos false.
+
+Por último, el tercer caso, y nuevamente por coerción de tipos, el valor 1 es un valor truthy, si lo negamos 2 veces, obtendremos true.
