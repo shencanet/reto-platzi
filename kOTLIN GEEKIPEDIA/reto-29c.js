1d70382fd86ec057727dcaeebd7407470d7c6178
shen <santi.dionis@gmail.com>
2023-05-29 11:59:36 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-29c.js
+++ kOTLIN GEEKIPEDIA/reto-29c.js
@@ -5,4 +5,6 @@ function getAge(...args) {
   
   getAge(21);
 
- 
+  Cuando usamos la sintaxis de ... en los parámetros de una función (ES6: Rest Operator) convertimos a dicho parámetro a un arreglo.
+
+  Entonces es tentador marcar la opción B: "array" pero esto sería un error de novato. En JavaScript no existe el tipo de dato array, para tipos no primitivos el lenguaje los evalua como object. Por ese motivo la respuesta correcta es la opción C: "object".
