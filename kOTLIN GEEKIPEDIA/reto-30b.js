89334b1cafb0b67034061f051b3fb597c18b4230
shen <santi.dionis@gmail.com>
2023-05-30 13:14:54 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-30b.js
+++ kOTLIN GEEKIPEDIA/reto-30b.js
@@ -3,3 +3,15 @@ const { 3: pera } = frutas;
 const [, , , pera2] = frutas; 
 console.log(pera);
 console.log(pera2);
+
+Para usar la desestructuración en arreglos es importante tener en cuenta los índices de los elementos. Por ello para acceder a Pera en el arreglo frutas hariamos algo como:
+
+const [, , , pera] = frutas;
+
+Donde cada , representa el salto de un índice del arreglo.
+
+Para una sistaxis mas breve podemos usar esto:
+
+const { 3:pera } = frutas;
+
+Donde el 3 representa las posiciones que deseamos saltar. Nota que aunque frutas sea un arreglo usamos {} para la desestructuración
