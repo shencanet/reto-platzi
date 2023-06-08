c17d16aa4624f1ad1f27b3bf80046bed7534faa9
shen <santi.dionis@gmail.com>
2023-06-08 20:26:46 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-08b.js
+++ kOTLIN GEEKIPEDIA/reto-08b.js
@@ -1,2 +1,9 @@
 const num = parseInt("7*6", 10);
 console.log(num); //?
+/*parseInt convierte un valor a tipo number de una base concreta (base binaria, octal, decimal, etc).
+
+En el ejemplo intentamos convertir "7*6" a base 10, osea, a base decimal.
+
+parseInt toma los valores validos de izquierda a derecha, dicho esto, solo tomará el valor 7 (el * y todo lo que le precede no es un valor valido para parseInt).
+
+En conclusión, solo convierte al 7 de string a number./*/
