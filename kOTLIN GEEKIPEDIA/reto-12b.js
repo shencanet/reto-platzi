b1626dc68979ab0a7f5be0fa609e8017da0bc86e
shen <santi.dionis@gmail.com>
2023-06-12 19:38:55 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-12b.js
+++ kOTLIN GEEKIPEDIA/reto-12b.js
@@ -2,3 +2,14 @@ const user = { name: 'Lydia', age: 21 };
 const admin = { admin: true, ...user };
 
 console.log(admin); //🤔?
+
+Respuesta Correcta: B) { admin: true, name: "Lydia", age: 21 }
+El spread operator en este ejemplo se encarga de propagar el objeto user dentro del objeto admin.
+
+Sin usar el spread operator tendríamos un objeto anidado:
+
+  { admin: true, { name: "Lydia", age: 21 } }  
+
+
+
+Justamente el spread operator se encarga de expandir user para evitar el anidamiento.
