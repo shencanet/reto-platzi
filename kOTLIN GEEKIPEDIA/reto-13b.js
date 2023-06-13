4c3a22b9269f5ecd7fd50c99bcbf6e76a0c23d71
shen <santi.dionis@gmail.com>
2023-06-13 21:06:08 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-13b.js
+++ kOTLIN GEEKIPEDIA/reto-13b.js
@@ -6,3 +6,5 @@ const settings = {
   
   const data = JSON.stringify(settings, ["level", "health"]);
   console.log(data); //🤔?
+
+  JSON.stringify puede recibir un 2do parámetro opcional denominado replacer, puede ser una función o un arreglo, y se encarga de hacer un filtro de las propiedades del objeto que deseamos convertir a string, en el ejemplo solo deseamos convertir las propiedades ["level", "health"], ignorando username.
