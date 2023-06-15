ac14d10dcf0cb365b38a3e2a846fd0ef4d543df9
shen <santi.dionis@gmail.com>
2023-06-15 20:49:28 +0200

    ADD: Retos

--- kOTLIN GEEKIPEDIA/reto-15b.js
+++ kOTLIN GEEKIPEDIA/reto-15b.js
@@ -8,3 +8,14 @@ multiply(); //🤔?
 multiply(); //🤔?
 multiply(value); //🤔?
 multiply(value); //🤔?
+
+/*Respuesta Correcta: C) 20, 20, 20, 40
+Hay que concentrarse en el orden en que se llaman las funciones para comprender que es lo que pasa acá.
+
+Primera llamada: A multiply no le pasamos ningún parámetro, por ende, toma el parámetro por defecto x que es un objeto desestructurado cuya key number tiene el valor de 10. Entonces x.number *= 2 nos retorna 20.
+
+Segunda llamada: Similar a la primera llamada, hacemos lo mismo, entonces obtenemos nuevamente 20.
+
+Tercera llamada: A multiply en su llamada le pasamos el argumento value por lo que la función ahora ignora el parámetro por defecto. number es nuevamente 10, por ello el resultado de la multiplicación nuevamente será 20.
+
+Cu
