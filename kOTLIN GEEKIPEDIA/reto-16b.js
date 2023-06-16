fdcfb350969a0c86b991be94b3bb7b33806f1385
shen <santi.dionis@gmail.com>
2023-06-16 21:02:57 +0200

    ADD: Retos

---
+++ kOTLIN GEEKIPEDIA/reto-16b.js
@@ -0,0 +1,4 @@
+console.log(Number(2) === Number(2)); //🤔?
+console.log(Boolean(false) === Boolean(false)); //🤔?
+console.log(Symbol('foo') === Symbol('foo')); //🤔?
+console.log(undefined === undefined); //🤔?
